7165a57fec6602348fa43af8ed178e17bd3af2ce
Alvin Zhu <alvinzhu33@yahoo.com>
2015-12-16 17:57:52 -0500

    Working weather converter complete

---
+++ LabGUI/Driver.java
@@ -0,0 +1,6 @@
+public class Driver{
+    public static void main(String[] args) {
+        TempConversionWindow w = new TempConversionWindow();
+        w.setVisible(true);
+    }
+}
--- LabGUI/TempConversionWindow.java
+++ LabGUI/TempConversionWindow.java
@@ -3,18 +3,11 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class TempConversionWindow extends JFrame implements ActionListener{
-    public static double CtoF(double t){
-	return t*9.0/5.0+32.0;
-    }
-    public static double FtoC(double t){
-	return (t-32.0)*5.0/9.0;
-    }
-
     private Container pane;
     private JLabel j;
     private JTextField t;
 
-    public Window() {
+    public TempConversionWindow(){
 	this.setTitle("Weather Conversion");
 	this.setSize(600,400);
 	this.setLocation(100,100);
@@ -23,34 +16,41 @@ public class TempConversionWindow extends JFrame implements ActionListener{
 	pane = this.getContentPane();
 	pane.setLayout(new BoxLayout(pane, BoxLayout.Y_AXIS));
 
-	JLabel description = new JLabel("Type in the degrees and click the button that corresponds with it");
+	JLabel description = new JLabel("Type in the degree and click a convert button");
 
-	t = new JTextField(10);
+	t = new JTextField();
 
-	JButton ctof = new JButton("Celius to Fahreinheit");
-	b.addActionListener(this);
-	b.setActionCommand("CtoF");
+	JButton ctof = new JButton("Celius to Fahrenheit");
+	ctof.addActionListener(this);
+	ctof.setActionCommand("CtoF");
 	JButton ftoc = new JButton("Fahrenheit to Celius");
-	b2.addActionListener(this);
-	b2.setActionCommand("FtoC");
+	ftoc.addActionListener(this);
+	ftoc.setActionCommand("FtoC");
 
 	j = new JLabel("Converted degrees here");
 	pane.add(description);
 	pane.add(t);
 	pane.add(ctof);
 	pane.add(ftoc);
-	pan.add(j);
+	pane.add(j);
+    }
+
+    public static double CtoF(double t){
+	return t*9.0/5.0+32.0;
+    }
+    public static double FtoC(double t){
+	return (t-32.0)*5.0/9.0;
     }
 
     public void actionPerformed(ActionEvent e){
 	String event = e.getActionCommand();
 	if(event.equals("CtoF")){
-	    String s = t.getText();
-	    j.setText(CtoF(s));
+	    Double s = Double.parseDouble(t.getText());
+	    j.setText(String.valueOf(CtoF(s)) + " degree Fahrenheit");
 	}
 	if(event.equals("FtoC")){
-	    String s = t.getText();
-	    j.setText(FtoC(s));
+	    Double s = Double.parseDouble(t.getText());
+	    j.setText(String.valueOf(FtoC(s)) + " degree Celsius");
 	}
     }
  
