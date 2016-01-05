4d03ee4cfcdee563ba1a7d643c7e0b540de5c079
Alvin Zhu <alvinzhu33@yahoo.com>
2016-01-04 22:10:35 -0500

    Made Driver better, using larger test

--- hwSorts/Driver.java
+++ hwSorts/Driver.java
@@ -1,11 +1,17 @@
+import java.util.*;
+
 public class Driver{
     public static void main(String[]args){
-	int[] i1= new int[]{6,2,7,1,2,6};
-	int[] i2= new int[]{-12,6,2,6,73,174,23,8,2,-11,73,-111};
+	int[] tester= new int[1000];
+	Random ran = new Random(1);
+	for(int x=0; x<tester.length; x++){
+	    tester[x]=ran.nextInt();
+	}
 
-	Sorts.selectionSort(i1);
-	Sorts.printArray(i1);
-	Sorts.selectionSort(i2);
-	Sorts.printArray(i2);
+	Sorts.printArray(tester);
+	Sorts.selectionSort(tester);
+	Sorts.insertionSort(tester);
+	Arrays.sort(tester);
+	Sorts.printArray(tester);
     }
 }
