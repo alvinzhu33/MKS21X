b2de9b7cba3d861150ee300ae223419558b16234
Alvin Zhu <alvinzhu33@yahoo.com>
2016-01-05 12:13:39 -0500

    Started bubble sort

--- hwSorts/Sorts.java
+++ hwSorts/Sorts.java
@@ -52,21 +52,6 @@ public class Sorts{
 
     //My Code
     public static void insertionSort(int[]data){
-	/*int[] copy= new int[data.length];
-	copy[0]=data[0];
-	if(data[1]<=copy[0]){
-	    copy[1]=copy[0];
-	    copy[0]=data[1];
-	}
-	else{
-	    copy[1]=data[1];
-	}
-	for(int i=2; i<data.length; i++){
-	    copy=add(copy, data[i]);
-	}
-	for(int i=0; i<copy.length; i++){
-	    data[i]=copy[i];
-	    }*/
 	for(int index=1; index<data.length; index++){
 	    int value=data[index];
 	    int swapIndex =index;
@@ -91,18 +76,6 @@ public class Sorts{
 	}
 	}*/
 
-    /*Mr.K's code
-    public static void insertionSort(int[] data){
-	for(int i=1; i<data.length; i++){
-	    int current=data[i];
-	    int j =i;
-	    while(j>0 && data[j-1]>current){
-		data[j]=data[j-1];
-		j--;
-	    }
-	    data[j]=current;
-	}
-	}*/
 
     public static void selectionSort(int[] data){
 	for(int index=0; index<data.length; index++){
@@ -118,6 +91,14 @@ public class Sorts{
 	    data[index]=next;
 	    data[storeIndex]=storeValue;
 	    printArray(data);
+	}
+    }
+
+    public static void bubbleSort(int[]data){
+	for(int x=0; x<data.length; x++){
+	    for(int y=x; y<data.length-x; y--){
+		if(data[y]<
+	    }
 	}
     }
 }
