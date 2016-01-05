ceed5104c88e838bbd59ac324e4aad26de8913e0
Alvin Zhu <alvinzhu33@yahoo.com>
2016-01-04 22:12:06 -0500

    Added smaller test for Driver

--- hwSorts/Driver.java
+++ hwSorts/Driver.java
@@ -13,5 +13,12 @@ public class Driver{
 	Sorts.insertionSort(tester);
 	Arrays.sort(tester);
 	Sorts.printArray(tester);
+
+	int[] i1= new int[]{-12,6,2,6,73,174,23,8,2,-11,73,-111};
+	Sorts.printArray(i1);
+	Sorts.selectionSort(i1);
+	Sorts.insertionSort(i1);
+	Arrays.sort(i1);
+	Sorts.printArray(i1);
     }
 }
