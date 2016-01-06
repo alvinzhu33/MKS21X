c2955764687a4a0a4c9005d6987fa5da72e4f6b0
Alvin Zhu <alvinzhu33@yahoo.com>
2016-01-06 18:20:58 -0500

    Filled in Test.sh

---
+++ hwSorts/Test.sh
@@ -0,0 +1,86 @@
+echo ---Testing Start---
+
+echo -insertionSort
+echo --insertion random
+java Driver2 insertion 10 random
+java Driver2 insertion 50 random
+java Driver2 insertion 100 random
+java Driver2 insertion 1000 random
+java Driver2 insertion 20000 random
+java Driver2 insertion 40000 random
+java Driver2 insertion 80000 random
+echo --insertion sorted
+java Driver2 insertion 10 sorted
+java Driver2 insertion 50 sorted
+java Driver2 insertion 100 sorted
+java Driver2 insertion 1000 sorted
+java Driver2 insertion 20000 sorted
+java Driver2 insertion 40000 sorted
+java Driver2 insertion 80000 sorted
+echo --insertion reversed
+java Driver2 insertion 10 reversed
+java Driver2 insertion 50 reversed
+java Driver2 insertion 100 reversed
+java Driver2 insertion 1000 reversed
+java Driver2 insertion 20000 reversed
+java Driver2 insertion 40000 reversed
+java Driver2 insertion 80000 reversed
+echo -End of insertionSort-
+echo ---
+
+echo -Selection Sort
+echo --selection random
+java Driver2 selection 10 random
+java Driver2 selection 50 random
+java Driver2 selection 100 random
+java Driver2 selection 1000 random
+java Driver2 selection 20000 random
+java Driver2 selection 40000 random
+java Driver2 selection 80000 random
+echo --selection sorted
+java Driver2 selection 10 random
+java Driver2 selection 50 random
+java Driver2 selection 100 random
+java Driver2 selection 1000 random
+java Driver2 selection 20000 random
+java Driver2 selection 40000 random
+java Driver2 selection 80000 random
+echo --selection reversed
+java Driver2 selection 10 reversed
+java Driver2 selection 50 reversed
+java Driver2 selection 100 reversed
+java Driver2 selection 1000 reversed
+java Driver2 selection 20000 reversed
+java Driver2 selection 40000 reversed
+java Driver2 selection 80000 reversed
+echo -End of selectionSort-
+echo ---
+
+echo -bubbleSort
+echo --bubble random
+java Driver2 bubble 10 random
+java Driver2 bubble 50 random
+java Driver2 bubble 100 random
+java Driver2 bubble 1000 random
+java Driver2 bubble 20000 random
+java Driver2 bubble 40000 random
+java Driver2 bubble 80000 random
+echo --bubble sorted
+java Driver2 bubble 10 random
+java Driver2 bubble 50 random
+java Driver2 bubble 100 random
+java Driver2 bubble 1000 random
+java Driver2 bubble 20000 random
+java Driver2 bubble 40000 random
+java Driver2 bubble 80000 random
+echo --bubble reversed
+java Driver2 bubble 10 random
+java Driver2 bubble 50 random
+java Driver2 bubble 100 random
+java Driver2 bubble 1000 random
+java Driver2 bubble 20000 random
+java Driver2 bubble 40000 random
+java Driver2 bubble 80000 random
+echo -End of bubbleSort
+
+echo ---End of Test---
